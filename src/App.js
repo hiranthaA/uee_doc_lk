1ac763aa85bd3b30b9b1aa4ab93fa1625f15c66b
hiranthaA <30893182+hiranthaA@users.noreply.github.com>
2018-09-24 21:33:53 +0530

    unncessasary files deleted

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Main from './main';
 import MainContainer from './components/maincontainer';
 
 class App extends Component {
