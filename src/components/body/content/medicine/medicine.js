134681e15d938a336338be53839a846bbe03da1c
hiranthaA <30893182+hiranthaA@users.noreply.github.com>
2018-09-25 02:36:43 +0530

    bug fix
    radio button default selection
    file upload dialog cancel -
     exception
    form validations

--- src/components/body/content/medicine/medicine.js
+++ src/components/body/content/medicine/medicine.js
@@ -10,23 +10,111 @@ class Medicine extends Component {
         super(props);
         this.handleChange = this.handleChange.bind(this);
         this.loadDeliveryDetails = this.loadDeliveryDetails.bind(this);
+        this.validate=this.validate.bind(this);
+
         this.state = {
             file: null,
-            deleverydetailtype: "frmacc"
+            deleverydetailtype: "frmacc",
+            selectedOption : "radioacc",
+            name : null,
+            contact : null,
+            address : null,
+            note : null
         }
     }
 
+    componentDidCatch(error, info) {
+        // Display fallback UI
+        // this.setState({ hasError: true });
+        // You can also log the error to an error reporting service
+        // logErrorToMyService(error, info);
+        console.log("error occured in this page");
+      }
+
     handleChange(event) {
-        this.setState({ file: URL.createObjectURL(event.target.files[0]) });
+        try {
+            this.setState({ file: URL.createObjectURL(event.target.files[0]) });
+        } catch (error) {
+            console.log("error");
+        }
+            
+    }
+
+    validateName() {
+        var name = document.getElementById("othername").value;
+        console.log(name);
+        var nameok = true;
+        if (name === null || name === "") {
+            console.log("name false");
+            nameok = false;
+        }
+        return nameok;
+    }
+
+    validateContact() {
+        var contact = document.getElementById("othercontact").value;
+        console.log(contact);
+        var contactok = true;
+        if (contact === null || contact === "" || contact.length != 10) {
+            console.log("contact false");
+            contactok = false;
+        }
+        return contactok;
+    }
+
+    validateAddress() {
+        var address = document.getElementById("otheraddress").value;
+        console.log(address);
+        var addressok = true;
+        if (address === null || address === "") {
+            console.log("address false");
+            addressok = false;
+        }
+        return addressok;
+    }
+
+    validatePrescription() {
+        var prescription = document.getElementById("uploadpress").value;
+        console.log(prescription);
+        var pressfileok = true;
+        if (prescription === null || prescription === "") {
+            console.log("presscription false");
+            pressfileok = false;
+        }
+        return pressfileok;
+    }
+
+    validate(){
+        if(this.state.deleverydetailtype==="frmacc"){
+            if(this.validatePrescription()){
+                window.$('#dlgSubmitSucces').modal('show');
+            }
+            else{
+                window.$('#dlgSubmitFailed').modal('show');
+            }
+        }
+        else if(this.state.deleverydetailtype==="frmother"){
+            if(this.validateName() & this.validateContact() & this.validateAddress() & this.validatePrescription()){
+                window.$('#dlgSubmitSucces').modal('show');
+            }
+            else{
+                window.$('#dlgSubmitFailed').modal('show');
+            }
+        }
+        else{
+            window.$('#dlgSubmitFailed').modal('show');
+        }
     }
 
     loadDeliveryDetails(e) {
         console.log("this is load delivery" + e.target.id);
         if (e.target.id === "radioacc") {
             this.setState({ deleverydetailtype: "frmacc" });
+            this.setState({ selectedOption : e.target.id});
         }
         else if (e.target.id === "radioother") {
             this.setState({ deleverydetailtype: "frmother" });
+            this.setState({ selectedOption : e.target.id});
         }
     }
 
@@ -46,12 +134,12 @@ class Medicine extends Component {
             );
         }
 
-        if(this.state.file===null){
+        if (this.state.file === null) {
             prescription_preview = null;
         }
-        else{
+        else {
             prescription_preview = (
-                < PressPreview preview={this.state.file}/>
+                < PressPreview preview={this.state.file} />
             )
         }
 
@@ -94,14 +182,14 @@ class Medicine extends Component {
                                                     <div className="col-xl-4, col-lg-4 col-md-4 col-sm-4 col-7 text-left">
                                                         <div className="form-check-inline">
                                                             <label class="form-check-label active">
-                                                                <input type="radio" class="form-check-input " id="radioacc" name="optradio" onClick={this.loadDeliveryDetails} />From Account
+                                                                <input type="radio" class="form-check-input" id="radioacc" name="optradio" checked={this.state.selectedOption === 'radioacc'} onClick={this.loadDeliveryDetails} />From Account
                                                             </label>
                                                         </div>
                                                     </div>
                                                     <div className="col-xl-8, col-lg-8 col-md-8 col-sm-8 col-5 text-left">
                                                         <div className="form-check-inline">
                                                             <label class="form-check-label">
-                                                                <input type="radio" class="form-check-input" id="radioother" name="optradio" onClick={this.loadDeliveryDetails} />Other
+                                                                <input type="radio" class="form-check-input" id="radioother" name="optradio" checked={this.state.selectedOption === 'radioother'} onClick={this.loadDeliveryDetails} />Other
                                                             </label>
                                                         </div>
                                                     </div>
@@ -109,7 +197,7 @@ class Medicine extends Component {
                                             </div>
                                         </div>
                                         {/* radio ends*/}
-                                        <hr/>
+                                        <hr />
                                         {loaddeliverydetails}
                                     </div>
                                 </div>
@@ -123,7 +211,7 @@ class Medicine extends Component {
                                             <div className="col-xl-12, col-lg-12 col-md-12 col-sm-12 col-12">
                                                 <div className="row">
                                                     <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                                        <input type="file" class="form-control-file border" onChange={this.handleChange} />
+                                                        <input type="file" class="form-control-file border" id="uploadpress" onChange={this.handleChange} />
                                                     </div>
                                                 </div>
                                                 <br />
@@ -138,22 +226,61 @@ class Medicine extends Component {
                         </div>
                     </div>
                 </div>
-                <br/>
+                <br />
                 {/* Submit button starts */}
                 <div className="row">
                     <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                        <button type="button" className="btn btnsubmit btn-success">Submit</button>
+                        <button type="button" className="btn btnsubmit btn-success" onClick={this.validate}>Submit</button>
                     </div>
                 </div>
                 <br />
                 {/* disclaimer starts */}
-                <Disclaimer/>
+                <Disclaimer />
                 {/* end of content */}
+
+                {/* Submit Success Dialog */}
+                <div className="modal fade" id="dlgSubmitSucces" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title text-success" id="exampleModalLabel">Success</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body text-success">
+                                Successfully Submitted!
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-success" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* Submit Fail Dialog */}
+                <div className="modal fade" id="dlgSubmitFailed" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title text-danger" id="exampleModalLabel">Failed</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body text-danger">
+                                Failed to Submit. Validation Failed!
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
 }
-            
-            
-            
+
+
+
 export default Medicine;
