24ec27d0ba3008aadd90d4314e1355686f3f0fe6
Parakrama Shashitha <pshashitha@gmail.com>
2018-09-26 14:14:22 +0530

    Validations

--- src/components/body/content/refund/mobilerefund.js
+++ src/components/body/content/refund/mobilerefund.js
@@ -5,10 +5,51 @@ class MobileRefund extends Component {
     constructor(props) {
         super(props);
         this.handleBankSelect = this.handleBankSelect.bind(this);
+        this.validate = this.validate.bind(this);
         this.state = {
             clicked:"FALSE"
         }
     }
+    validate(){
+        var validated = false;
+        if(this.refValidate() & this.mobileNumValidate()){
+            validated =true;
+        }
+        if(validated!=true)
+            window.$('#bankRefundFailed').modal('show');
+        else
+            window.$('#bankRefundSuccess').modal('show');
+        return validated;
+    }
+    refValidate(){
+        var refNo = document.getElementById("refID").value;
+        if(refNo===""){
+            return false;
+        }
+        var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+
+        if(format.test(refNo)){
+            return false;
+        } else {
+            return true;
+        }
+    }
+    mobileNumValidate(){
+        var mobileNum = document.getElementById("mobileNum").value;
+        if(mobileNum===""){
+            return false;
+        }
+        var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+
+        if(format.test(mobileNum)){
+            return false;
+        } else {
+
+            return /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(mobileNum);
+            
+        }
+    }
+
     popupShow() {
         var popup = document.getElementById("myPopup");
         popup.classList.toggle("show");
@@ -82,7 +123,47 @@ class MobileRefund extends Component {
                 <hr/>
                 <br/><br/><br/><br/><br/><br/>
                 
-                <button type="submit" className="btn btn-lg btn-primary">Submit</button>
+                <button type="submit" className="btn btn-lg btn-primary" onClick={this.validate}>Submit</button>
+
+
+                {/* Submit Success Dialog */}
+                <div className="modal fade" id="bankRefundSuccess" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title text-success" id="exampleModalLabel">Success</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body text-success">
+                                Successfully Submitted!
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-success" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* Submit Fail Dialog */}
+                <div className="modal fade" id="bankRefundFailed" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title text-danger" id="exampleModalLabel">Failed</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body text-danger">
+                                Validation Failed!
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
